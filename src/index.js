ee3db68eb5cd6520b925b912c2d2a8a1f129adf9
drooz <farahkhaledsaleh@farahs-MacBook-Pro-4.local>
2020-09-12 05:14:09 +0300

    Migratee from RRF v2 to v3

--- src/index.js
+++ src/index.js
@@ -3,26 +3,34 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { reduxFirestore, getFirestore } from "redux-firestore";
-import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
-import firebaseConfig from "./config/firebaseConfig";
+import { createFirestoreInstance, getFirestore } from "redux-firestore";
+import { ReactReduxFirebaseProvider, getFirebase } from "react-redux-firebase";
+import firebase from "./config/firebaseConfig";
+
+const rrfConfig = { userProfile: "users" }; // react-redux-firebase config
 
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
-    reduxFirestore(firebaseConfig),
-    reactReduxFirebase(firebaseConfig)
-  )
+
+  applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase }))
 );
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance, // <- needed if using firestore
+};
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
   </Provider>,
   document.getElementById("root")
 );
